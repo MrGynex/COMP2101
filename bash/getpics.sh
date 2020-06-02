0c63cbcfe78163279ffb5e28aa866088358bb8cc
MrGynex <200416538@student.georgianc.on.ca>
2020-06-02 11:42:57 -0700

    getpics.sh section of lab. completed and confirmed working

---
+++ bash/getpics.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# this script puts some picture files into a Pictures directory in our home directory
+# it does some error testing
+# it summarizes the Pictures directory when it is done
+#
+
+# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
+#   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have
+# it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
+
+# make a Pictures directory if we don't have one - assumes we have a home directory
+test -d ~/Pictures || mkdir ~/Pictures
+
+# download a zipfile of pictures to our Pictures directory if it isn't already there - assumes you are online
+test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net/pics.zip
+
+# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
+#   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have
+# it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
+
+#these commands are for checking if the tar file is present, and if not acquiring it
+test -f ~/Pictures/pics.tgz || (wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz)
+
+# unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
+test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
+
+# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
+#   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have
+# it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
+
+#tests to see if the tar file is present. If the file is present then it is extracted and the tarball is removed.
+
+test -f ~/Pictures/pics.tgz && ( tar -xf ~/Pictures/pics.tgz -C ~/Pictures && rm ~/Pictures/pics.tgz)
+
+# Make a report on what we have in the Pictures directory
+test -d ~/Pictures && cat <<EOF
+Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
+The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
+EOF
